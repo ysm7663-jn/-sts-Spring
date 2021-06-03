eda37a87db41ce6743b980602c358c701786aa9a
ysm7663-jn <ysm7663@gmail.com>
2021-06-03 16:13:02 +0900

    04_bean Singer

---
+++ 04_bean/src/main/java/com/korea/spring/Singer.java
@@ -0,0 +1,42 @@
+package com.korea.spring;
+
+public class Singer {
+	
+	// field
+	private String name;
+	private Song song;
+	
+	// constructor
+	public Singer() {
+		
+	}
+	
+	public Singer(String name, Song song) {
+		this.name = name;
+		this.song = song;
+	}
+	
+
+	// method
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Song getSong() {
+		return song;
+	}
+
+	public void setSong(Song song) {
+		this.song = song;
+	}
+	
+	public void singerInfo() {
+		System.out.println("이름 : " + name);
+		song.songInfo();
+	}
+	
+}
