5044384bde69b59d071509adb2aae5aa8f93aa9b
ysm7663-jn <ysm7663@gmail.com>
2021-06-09 11:33:29 +0900

    06_bean

--- 06_bean/src/main/java/com/korea/spring/Appcontext.java
+++ 06_bean/src/main/java/com/korea/spring/Appcontext.java
@@ -8,17 +8,16 @@ import org.springframework.context.annotation.Configuration;
 // @Configuration : Bean을 생성하는 app-context.xml과 같은 일을 한다.
 @Configuration
 public class Appcontext {
-	
+
 	@Bean
 	public Person person2() {
 		ArrayList<String> hobbies = new ArrayList<String>();
 		hobbies.add("요리");
 		hobbies.add("독서");
 		hobbies.add("음악감상");
-		
+
 		return new Person("제임스", hobbies);
-				
+
 	}
-	
-	
+
 }
